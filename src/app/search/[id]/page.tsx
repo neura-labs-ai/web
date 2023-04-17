c9139c79ebd08bc788f41310e4f2e604c36fbe81
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-04-17 03:37:02 -0400

    Protected Routes

--- src/app/search/[id]/page.tsx
+++ src/app/search/[id]/page.tsx
@@ -1,13 +1,32 @@
-import { FC } from 'react'
+import NotAuthorized from "@/components/NotAuthorized";
+import { isAuthenticated, returnToLogin } from "@/lib/utils";
+import { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { FC } from "react";
 
 interface pageProps {}
 
-const page: FC<pageProps> = ({}) => {
-   return (
-       <>
-           <div>page</div>
-       </>
-   )
+export async function generateMetadata({}: pageProps): Promise<Metadata> {
+	const session = await getServerSession();
+
+	if (!isAuthenticated(session)) return returnToLogin();
+
+	return {
+		title: `Search [ID]`, // todo - make the name of the library that was searched
+		description: `Search results for [ID]`,
+	};
 }
 
-export default page
+const page = async ({}) => {
+	const session = await getServerSession();
+
+	if (!isAuthenticated(session)) return <NotAuthorized />;
+
+	return (
+		<>
+			<div>page</div>
+		</>
+	);
+};
+
+export default page;
