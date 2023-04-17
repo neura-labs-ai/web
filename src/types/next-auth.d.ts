66ad218478bef662f7d842fe393d573e477bf9de
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-04-16 21:07:12 -0400

    adding usernames to schema

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -7,18 +7,76 @@ type UserId = string;
 declare module "next-auth/jwt" {
 	interface JWT {
 		id: UserId;
-		role: Role;
+		role: Role | null | undefined;
 	}
 }
 
 declare module "next-auth" {
 	interface Session {
-		user: User & {
-			id: UserId;
-		}
+		user: {
+			/**
+			 * The user's email address
+			 */
+			email?: string | null;
+
+			/**
+			 * The user's unique id number
+			 */
+			id?: string | null;
+
+			/**
+			 * The users preferred avatar.
+			 * Provided by GitHub
+			 */
+			image?: string | null;
+
+			/**
+			 * The user's full name
+			 */
+			name?: string | null;
+
+			/**
+			 * The user's custom & public username viewable to others
+			 */
+			username?: string | null;
+
+			/**
+			 * The user's role
+			 */
+			role?: Role | null;
+		};
 	}
 
 	interface User {
-		role: Role;
+		/**
+		 * The user's email address
+		 */
+		email?: string | null;
+
+		/**
+		 * The user's unique id number
+		 */
+		id: string;
+
+		/**
+		 * The users preferred avatar.
+		 * Usually provided by the user's OAuth provider of choice
+		 */
+		image?: string | null;
+
+		/**
+		 * The user's full name
+		 */
+		name?: string | null;
+
+		/**
+		 * The user's custom & public username viewable to others
+		 */
+		username?: string | null;
+
+		/**
+		 * The user's role
+		 */
+		role?: Role | null;
 	}
 }
