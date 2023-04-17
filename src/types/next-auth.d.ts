8bdd64f98918f8c3f7928554fb4f70e9a137f718
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-04-16 22:59:35 -0400

    updates

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,6 +1,6 @@
 import { Role } from "@prisma/client";
-import type { Session, User } from "next-auth";
 import type { JWT } from "next-auth/jwt";
+import type { Session } from "next-auth";
 
 type UserId = string;
 
@@ -8,55 +8,26 @@ declare module "next-auth/jwt" {
 	interface JWT {
 		id: UserId;
 		role: Role | null | undefined;
+		bio: string | null | undefined;
 	}
 }
 
 declare module "next-auth" {
 	interface Session {
-		user: {
-			/**
-			 * The user's email address
-			 */
-			email?: string | null;
-
-			/**
-			 * The user's unique id number
-			 */
-			id?: string | null;
-
-			/**
-			 * The users preferred avatar.
-			 * Provided by GitHub
-			 */
-			image?: string | null;
-
-			/**
-			 * The user's full name
-			 */
-			name?: string | null;
-
-			/**
-			 * The user's custom & public username viewable to others
-			 */
-			username?: string | null;
-
-			/**
-			 * The user's role
-			 */
-			role?: Role | null;
-		};
+		user: User;
 	}
 
+	// The user model is based off the github API data. This is extended  from the Auth.js User Mode.
+	// see - https://api.github.com/users/GitHub for an example user.
 	interface User {
-		/**
-		 * The user's email address
-		 */
-		email?: string | null;
-
 		/**
 		 * The user's unique id number
 		 */
 		id: string;
+		/**
+		 * The user's email address
+		 */
+		email?: string | null;
 
 		/**
 		 * The users preferred avatar.
@@ -65,7 +36,7 @@ declare module "next-auth" {
 		image?: string | null;
 
 		/**
-		 * The user's full name
+		 * The user's custom name
 		 */
 		name?: string | null;
 
@@ -78,5 +49,24 @@ declare module "next-auth" {
 		 * The user's role
 		 */
 		role?: Role | null;
+
+		/**
+		 * The users bio content
+		 */
+		bio?: string | null;
 	}
+}
+
+/**
+ * Custom profile object returned when a user account
+ * is saved into the database on sign-in.
+ * The full profile object can be found in ./server/gh_profile.json
+ */
+export interface GitHubProfile {
+	id: number;
+	login: string;
+	email: string;
+	avatar_url: string;
+	name?: string;
+	bio?: string;
 }
