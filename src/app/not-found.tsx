46da66034d5f6d90d038a0cff81dab7d53972489
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-04-20 17:27:26 -0400

    Update not-found.tsx
    fixing link not redirect

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,17 @@
-import Link from "next/link";
-
 export default function NotFound() {
   return (
     <div className="flex flex-col h-screen items-center justify-center bg-gray-100">
       <h1 className="text-2xl md:text-4xl font-bold text-gray-800 mb-8 text-center">
         404 - Page Not Found
       </h1>
-      <Link href={"/"}>
-        <div className="px-4 py-2 rounded-md bg-gray-800 text-white hover:bg-gray-900 text-sm md:text-base">
-          Return Home
-        </div>
-      </Link>
+      <a
+        href={"/"}
+        className={
+          "px-4 py-2 rounded-md bg-gray-800 text-white hover:bg-gray-900 text-sm md:text-base"
+        }
+      >
+        Return Home
+      </a>
     </div>
   );
 }
