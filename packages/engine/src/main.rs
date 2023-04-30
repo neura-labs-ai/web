365d30e90be04949e09fac02b499488d181f5449
ThatGuyJamal <43446907+ThatGuyJamal@users.noreply.github.com>
2023-04-30 19:33:21 -0400

    testing

--- packages/engine/src/main.rs
+++ packages/engine/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+extern crate anyhow;
+
+use rust_bert::pipelines::common::ModelType;
+use rust_bert::pipelines::translation::{Language, TranslationModelBuilder};
+use tch::Device;
+
+fn main() -> anyhow::Result<()> {
+    let model = TranslationModelBuilder::new()
+        .with_device(Device::cuda_if_available())
+        .with_model_type(ModelType::Marian)
+        // .with_large_model()
+        .with_source_languages(vec![Language::English])
+        .with_target_languages(vec![Language::Spanish])
+        .create_model()?;
+
+    let input_context_1 = "This is a sentence to be translated";
+    let input_context_2 = "The dog did not wake up.";
+
+    let output = model.translate(&[input_context_1, input_context_2], Language::English, Language::Spanish)?;
+
+    for sentence in output {
+        println!("{sentence}");
+    }
+    
+    Ok(())
 }
