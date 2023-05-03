966b7dc65fba43ca72d9038e69a381c04f1644f4
ThatGuyJamal <jamallyons8@gmail.com>
2023-05-03 11:43:21 -0400

    updated

--- website/src/components/internal/telemetry/VercelAnalytics.tsx
+++ website/src/components/internal/telemetry/VercelAnalytics.tsx
@@ -8,34 +8,39 @@ interface VercelAnalyticsProps {}
 
 /** see - https://vercel.com/docs/concepts/analytics/package */
 const VercelAnalytics: FC<VercelAnalyticsProps> = ({}) => {
-  return (
-    <Analytics
-      debug={process.env.NODE_ENV === "development"}
-      beforeSend={(e) => {
-        const url = new URL(e.url);
-        if (url.pathname.includes("/api/")) return null; // Don't track API requests
+	return (
+		<Analytics
+			debug={process.env.NODE_ENV === "development"}
+			beforeSend={(e) => {
+				const url = new URL(e.url);
+				if (url.pathname.includes("/api/")) return null; // Don't track API requests
 
-        const tel_enabled = localStorage.getItem(LOCAL_STORAGE_KEYS.TELEMETRY)
-        
-        // Only track events if the user has opted in
-        if(tel_enabled) {
+				const tel_enabled = localStorage.getItem(LOCAL_STORAGE_KEYS.TELEMETRY);
 
-          const enabled = JSON.parse(tel_enabled) as {
-            enabled: boolean;
-          }
+				// Only track events if the user has opted in
+				if (tel_enabled) {
+					const enabled = JSON.parse(tel_enabled) as {
+						enabled: boolean;
+					};
 
-          if(!enabled.enabled) return null;
+					if (!enabled.enabled) return null;
 
-          return {
-            ...e,
-            url: url.toString(),
-          };
-        }
+					return {
+						...e,
+						url: url.toString(),
+					};
+				}
 
-        return null;
-      }}
-    />
-  );
+        // todo - remove this
+				return {
+					...e,
+					url: url.toString(),
+				};
+
+        // return null;
+			}}
+		/>
+	);
 };
 
 export default VercelAnalytics;
